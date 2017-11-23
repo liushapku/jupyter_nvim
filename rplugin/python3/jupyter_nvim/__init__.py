486229a7d77833bc31de10f805c2cda3c4bdce0b
Sha Liu <liushapku@gmail.com>
2017-11-23 09:34:25 +0800

    __init__.py: add test nvim rplugin

--- rplugin/python3/jupyter_nvim/__init__.py
+++ rplugin/python3/jupyter_nvim/__init__.py
@@ -2,42 +2,49 @@
 version_info = (0, 1, 0)
 __version__ = '.'.join(map(str, version_info[:3])) + ''.join(version_info[3:])
 
-
+import os
 import neovim
-import zerorpc
+import logging
 from jupyter_nvim.nvimapp import (
     JupyterIOPubMessageHandler,
     JupyterNvimBufferApp,
     JupyterNvimApp,
 )
 
+logger = logging.getLogger(__name__)
+if 'NVIM_IPY_DEBUG_FILE' in os.environ:
+    logfile = os.environ['NVIM_IPY_DEBUG_FILE'].strip()
+    logger.addHandler(logging.FileHandler(logfile, 'w'))
+    logger.level = logging.DEBUG
+
+
 @neovim.plugin
-@neovim.encoding(True)
-class IPythonPlugin(object):
+# @neovim.encoding(True)
+class TestPlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
-        self.zerorpc = zerorpc.Client()
-        self.zerorpc.connect("ipc://jupyter-nvim")
-        self.zerorpc.print("Initialized")
+        logger.info('Initialized')
         # self.app = JupyterNvimApp()
         # self.app.initialize([])
 
-    @neovim.command('JupyterTestCommand')
+    @neovim.command('JupyterTestCommand', nargs="*", range=True)
     def testcommand(self, args, range):
         message = 'Command with args: {}, range: {}'.format(args, range)
         self.nvim.current.line = message
-        self.zerorpc.print(message)
+        logger.info(message)
+        # self.zerorpc.print(message)
 
     @neovim.function('JupyterTestFunction', sync=True)
-    def testfunction(self, args):
+    def testsyncfunction(self, args):
+        logger.info('test sync function: {}'.format(args))
         return args
 
     # allow_nested allows async functions to run when another sync function is being called
     # async function cannot return value
-    @neovim.function('JupyterTestAsyncNestedFunction', sync=False, allow_nested=True)
+    @neovim.function('JupyterTestAsyncNestedFunction', sync=False)
     def testfunction(self, args):
-        zerorpc.print('test async nested function')
+        logger.info('test async nested function {}'.format(args))
 
-    @shutdown_hook(f)
-    def shutdown_hook(f):
-        zerorpc.print('shuting down...')
+    @neovim.shutdown_hook
+    def shutdown_hook(self):
+        logger.print('shuting down...')
